fd1aba289f8adea3accb451ed489cc9a14152716
viratpaudel <viratpaudel365@gmail.com>
2026-08-27 22:12:26 +0530

    Create file_organizer.py

---
+++ file_organizer.py
@@ -0,0 +1,34 @@
+import os
+import shutil
+
+def organize(folder):
+    files = {
+        "Images": [".jpg", ".jpeg", ".png", ".gif"],
+        "Videos": [".mp4", ".mkv", ".avi"],
+        "Documents": [".pdf", ".docx", ".txt", ".xlsx"],
+        "Code": [".py", ".js", ".html", ".css"],
+        "Archives": [".zip", ".rar", ".7z"]
+    }
+
+    for file in os.listdir(folder):
+        file_path = os.path.join(folder, file)
+
+        if not os.path.isfile(file_path):
+            continue
+
+        ext = os.path.splitext(file)[1].lower()
+
+        for name, extensions in files.items():
+            if ext in extensions:
+                new_folder = os.path.join(folder, name)
+
+                if not os.path.exists(new_folder):
+                    os.mkdir(new_folder)
+
+                shutil.move(file_path, os.path.join(new_folder, file))
+                print(file, "->", name)
+                break
+
+folder = input("Folder: ")
+organize(folder)
+print("Done")
